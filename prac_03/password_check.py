f09057f2f0c5741e4f2f84932273490749dd9b75
HaijiaZhangCp1404 <1324044511@qq.com>
2022-03-30 16:05:14 +0800

    fifth edition

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,11 +1,11 @@
-def get_password(mininum_lendth):
-    password = str(input("Enter password: "))
-    while len(password) < mininum_lendth:
-        password = str(input("Enter password: "))
-    return password
 def main():
     password = get_password(10)
     for i in password:
         print('*',end='')
     print()
+def get_password(mininum_lendth):
+    password = str(input("Enter password: "))
+    while len(password) < mininum_lendth:
+        password = str(input("Enter password: "))
+    return password
 main()
