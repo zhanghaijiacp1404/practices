d04412bfd1f1bec4866144adf909f1b10fe1ac04
HaijiaZhangCp1404 <1324044511@qq.com>
2022-04-10 15:07:15 +0800

    update for prac_04

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,33 @@
+"""List object operation"""
+def main():
+    """Function body"""
+    numbers = [3, 1, 4, 1, 5, 9, 2]
+    '''
+    numbers[0]: 3
+    numbers[-1]: 2
+    numbers[3]: 1
+    numbers[:-1]: [3 1 4 1 5 9 ]
+    numbers[3:4]: [1]
+    5 in numbers: True
+    7 in numbers: False
+    "3" in numbers: False
+    numbers + [6, 5, 3]: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+    '''
+
+    """Testing for above operations"""
+    print(numbers[0])
+    print(numbers[-1])
+    print(numbers[3])
+    print(numbers[:-1])
+    print(numbers[3:4])
+    print(5 in numbers)
+    print(7 in numbers)
+    print("3" in numbers)
+    print(numbers + [6, 5, 3])
+
+    numbers[0] = "ten"
+    numbers[-1] = 1
+    numbers = numbers[2:-1]
+    print(9 in numbers)
+if __name__ == '__main__':
+    main()
