1fd7a6f711dd35a9971301818698e92900946c90
HaijiaZhangCp1404 <1324044511@qq.com>
2022-04-12 10:28:24 +0800

    Thrid update for prac_04

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -29,12 +29,16 @@ print(a_names)
 
 # TODO: use a list comprehension to create a list of all of the full_names
 # in lowercase format
-# lowercase_full_names =
+lower_case_full_names = [full_name.lower() for full_name in full_names]
+print(lower_case_full_names)
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # TODO: use a list comprehension to create a list of integers
 # from the above list of strings
-# numbers =
+numbers = [int(str_number) for str_number in almost_numbers]
+print(numbers)
 
 # TODO: use a list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
+numbers = [number for number in numbers if number > 9]
+print(numbers)
