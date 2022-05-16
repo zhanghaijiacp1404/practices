c4728a9eea3c9d47343691a7cc34b161fddbc84a
HaijiaZhangCp1404 <1324044511@qq.com>
2022-05-16 14:15:19 +0800

    Add doctrings and comments

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -1,17 +1,22 @@
+"""Testing Taxi object"""
 from prac_08.taxi import Taxi
 
 
 def main():
+    """Start program"""
+    # Initialize a Taxi instance
     prius_1 = Taxi(name = "Prius 1", fuel=100.0)
     prius_1.drive(40)
     print(prius_1)
-    print(f"Prius 1's fare is: ${prius_1.get_fare()}")
+    # Print the taxi's current fare
+    print(f"Prius 1's current fare is: ${prius_1.get_fare()}")
 
+    # Start a new fare
     prius_1.start_fare()
     prius_1.drive(100)
     print("")
     print(prius_1)
-    print(f"Prius 1's fare is: ${prius_1.get_fare()}")
+    print(f"Prius 1's current fare is: ${prius_1.get_fare()}")
 
 
 if __name__ == '__main__':
