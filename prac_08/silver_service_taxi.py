9829734cfe6e3e2076eb6003960ee1676c0a0b0d
HaijiaZhangCp1404 <1324044511@qq.com>
2022-05-16 14:10:28 +0800

    Add doctrings for taxi_simulator.py and fix silver_service_taxi.py

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -1,16 +1,22 @@
+"""SilverServiceTaxi class"""
 from prac_08.taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
+    """Representing a SilverServiceTaxi object"""
     flagfall = 4.5
 
-    def __init__(self,fanciness, **kwargs):
+    def __init__(self, fanciness, **kwargs):
+        """Initialize a SilverServiceTaxi instance"""
         super().__init__(**kwargs)
+        # Multiply the fanciness variable to price_per_km variable
         self.price_per_km *= fanciness
         self.fanciness = fanciness
 
     def get_fare(self):
+        """return the fare for silver service taxi"""
         return super().get_fare() + self.flagfall
 
     def __str__(self):
+        """Return a string representation of a SilverServiceTaxi object"""
         return super().__str__() + f" plus flagfall of ${self.flagfall}"
--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -1,34 +1,45 @@
+"""Taxi simulator with traditional taxis and silver service taxis"""
 from prac_08.taxi import Taxi
 from prac_08.silver_service_taxi import SilverServiceTaxi
 
+# A menu for user to choose
 MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def display_taxis(taxis):
+    """Dis play the detail information for each object in the list"""
     for index, taxi in enumerate(taxis, 0):
         print(f"{index} - {taxi}")
 
 
 def main():
+    """Start program"""
+    # Initialize a list contains Taxi object and SilverServiceTaxi objects
     taxis = [Taxi(name="Prius", fuel=100),
              SilverServiceTaxi(name="Limo", fuel=100, fanciness=2),
              SilverServiceTaxi(name="Hummer", fuel=200, fanciness=4)]
+    # Represent the user's total bill for driving
     bill_to_date = 0
+    # Save the temporary object
     temp_taxi = None
     print("Let's drive!")
     print(MENU)
     user_choice = input(">>> ").lower()
     while user_choice != 'q':
         if user_choice == 'c':
+            # Choose a taxi
             print("Taxis available")
             display_taxis(taxis)
             taxi_choice = int(input("Choose taxi: "))
+            # Check the validation of user's choice of taxi by IndexError exception
             try:
                 temp_taxi = taxis[taxi_choice]
             except IndexError:
                 print("Invalid taxi choice")
         elif user_choice == 'd':
+            # Start driving
             if not temp_taxi:
+                # Promote the user with a message if no taxis haven been chosen
                 print("You need to choose a taxi before you can drive")
             else:
                 temp_taxi.start_fare()
@@ -41,7 +52,9 @@ def main():
         print(f"Bill to date: ${bill_to_date:.2f}")
         print(MENU)
         user_choice = input(">>> ").lower()
+    # Print total cost for the trip
     print(f"Total trip cost: ${bill_to_date:.2f}")
+    # Finally display the detailed information for all taxis
     print("Taxis are now:")
     display_taxis(taxis)
 
