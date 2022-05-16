ebf27376434095d41a946ae44d6289bc1c608d99
HaijiaZhangCp1404 <1324044511@qq.com>
2022-05-16 14:16:42 +0800

    Fix string formatting

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -9,14 +9,14 @@ def main():
     prius_1.drive(40)
     print(prius_1)
     # Print the taxi's current fare
-    print(f"Prius 1's current fare is: ${prius_1.get_fare()}")
+    print(f"Prius 1's current fare is: ${prius_1.get_fare():.2f}")
 
     # Start a new fare
     prius_1.start_fare()
     prius_1.drive(100)
     print("")
     print(prius_1)
-    print(f"Prius 1's current fare is: ${prius_1.get_fare()}")
+    print(f"Prius 1's current fare is: ${prius_1.get_fare():.2f}")
 
 
 if __name__ == '__main__':
