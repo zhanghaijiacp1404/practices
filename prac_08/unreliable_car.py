dc60e3cb1c85a827dd2559d0472bf963a5dff439
HaijiaZhangCp1404 <1324044511@qq.com>
2022-05-16 14:25:11 +0800

    Fix syntax for drive(self, distance) method and comments

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -6,18 +6,18 @@ from random import randint as ri
 class UnreliableCar(Car):
     """An UnreliableCar object"""
 
-    def __init__(self, name, fuel, reliability=0):
+    def __init__(self, name, fuel, reliability):
         """Initialize an UnreliableCar instance"""
         super().__init__(name, fuel)
         # A float variable between 0 and 100 inclusive
-        self.reliability = reliability
+        self.reliability = float(reliability)
 
     def drive(self, distance):
         """
-        Drive this UnreliableCar and return the distance if the car's reliability is larger than a random integer
+        Drive this UnreliableCar and return the distance if a random integer is less than reliability
         Otherwise return 0
         """
-        if self.reliability > ri(0, 100):
+        if ri(0, 100) < self.reliability:
             return super().drive(distance)
         else:
             return 0
--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -4,7 +4,7 @@ from prac_08.unreliable_car import UnreliableCar
 
 def main():
     """Start program"""
-    test_car = UnreliableCar(name="UnreliableCar", fuel=100, reliability=50)
+    test_car = UnreliableCar(name="UnreliableCar", fuel=100, reliability=90)
     # Test the drive(self, distance) method in UnreliableCar object
     print(test_car.drive(2))
     # Test the __str__(self) method in UnreliableCar object
