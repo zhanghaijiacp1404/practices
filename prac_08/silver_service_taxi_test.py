5ea5683798b514847076eb12a1fb231d2dee4d42
HaijiaZhangCp1404 <1324044511@qq.com>
2022-05-16 14:38:35 +0800

    Fix comments and add a line for testing __str__(self) method

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -1,10 +1,16 @@
+"""Testing for SilverServiceTaxi object"""
 from prac_08.silver_service_taxi import SilverServiceTaxi
 
 
 def main():
+    """Start program"""
     Hummer = SilverServiceTaxi(name="Hummer", fuel=200, fanciness=2)
+    # Start driving this taxi
     Hummer.drive(distance=18)
-    print(Hummer.get_fare())
+    # Print the current fare
+    print(f"Current fare: ${Hummer.get_fare():.2f}")
+    # Print the string representation of the Hummer object
+    print(Hummer)
 
 
 if __name__ == '__main__':
