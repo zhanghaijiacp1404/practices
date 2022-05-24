23c1c56f12118e53e0b8f0e48aaf300f45e5d7ba
HaijiaZhangCp1404 <1324044511@qq.com>
2022-05-24 20:08:51 +0800

    Fix the code implementation by using dictionary instead of list

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -15,18 +15,25 @@ def main():
     # Check the working directory again
     print(os.getcwd())
 
+    # A dictionary variable indicates: file suffix - file category
+    file_category = {}
+
     for file in os.listdir('.'):
+        # If system detect the directory, ignore it. Only focus on files
+        if os.path.isdir(file):
+            continue
+        suffix = file.split('.')[-1]
         # Only handle the file if that file exists
-        if os.path.isfile(file):
-            file_type = os.path.splitext(file)[1].replace('.', '')
-            category = input(f"What category would you like to sort {file_type} files into? ")
+        if suffix not in file_category:
+            category = input(f"What category would you like to sort {suffix} files into? ")
+            file_category[suffix] = category
             # Only create the user-defined directory when that directory doesn't exists. Avoid crashing
             try:
                 os.mkdir(category)
             except FileExistsError:
                 pass
             # Move the file to their belonging directories
-            shutil.move(file, category + "\\")
+        shutil.move(file, category + "\\")
 
 
 if __name__ == '__main__':
