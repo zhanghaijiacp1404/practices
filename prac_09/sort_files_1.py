ae841999cdb122f0da4e0bcc76c99c2e061c91df
HaijiaZhangCp1404 <1324044511@qq.com>
2022-05-24 18:42:27 +0800

    Add docstrings and more comments

--- prac_09/sort_files_1.py
+++ prac_09/sort_files_1.py
@@ -1,25 +1,38 @@
+"""
+A file sorting demo to classify the types of
+different files based on their suffix like:
+.doc .docx .txt .png .jpg .xls and so on.
+Also, based on the files' suffix, create
+directories and move the files to their belong
+directory.
+"""
 import os
 import shutil
 
 
 def main():
+    """Start program."""
+    # Check working directory
     print(os.getcwd())
     # Set path
     os.chdir(os.getcwd() + "\\FilesToSort\\")
 
     files = []
-    for filename in os.listdir("."):
-        files.append(filename)
+    for file in os.listdir("."):
+        files.append(file)
     print(files)
 
     # Create sub directories base on the files' type
     # And move files to there belonging type directory
-    for filename in files:
-        if os.path.isfile(filename):
-            file_type = os.path.splitext(filename)[1].replace(".", "")
+    for file in files:
+        # Only handling the file if that file exists
+        if os.path.isfile(file):
+            file_type = os.path.splitext(file)[1].replace(".", "")
+            # Only create the directory when that directory doesn't exists. Avoid crashing
             if not os.path.isdir(file_type):
                 os.mkdir(file_type)
-            shutil.move(filename, file_type + '\\')
+            # Move the file to their belonging directory
+            shutil.move(file, file_type + '\\')
 
 
 if __name__ == '__main__':
--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -1,18 +1,29 @@
+"""
+Modified version for sort_files_1.py
+Create directories (also the target for file moving)
+based on user's classification.
+"""
 import os, shutil
 
 
 def main():
+    """Start program."""
     # Check and set working directory
     print(os.getcwd())
+    # Set the correct working directory
     os.chdir(os.getcwd() + "\\FilesToSort\\")
+    # Check the working directory again
     print(os.getcwd())
 
     for file in os.listdir('.'):
+        # Only handle the file if that file exists
         if os.path.isfile(file):
             file_type = os.path.splitext(file)[1].replace('.', '')
             category = input(f"What category would you like to sort {file_type} files into? ")
+            # Only create the user-defined directory when that directory doesn't exists. Avoid crashing
             if not os.path.isdir(category):
                 os.mkdir(category)
+            # Move the file to their belonging directories
             shutil.move(file, category + "\\")
 
 
