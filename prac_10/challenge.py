bd6d0642a6949c54ab21486cb0e13eb370b9073e
HaijiaZhangCp1404 <1324044511@qq.com>
2022-05-30 19:45:00 +0800

    Add two routes' function

--- prac_10/challenge.py
+++ prac_10/challenge.py
@@ -11,10 +11,20 @@ def celsius_to_fahrenheit(celsius):
 app = Flask(__name__)
 
 
-# Setup app's route
+# Setup app's route and its corresponding web content
+@app.route("/")
+def welcome():
+    return "<h1>Welcome to Celsius to Fahrenheit Demo Web! " \
+           "Enter \"f\" to the route to see more information</h1>"
+
+
 @app.route('/f')
+def promote_user():
+    return "<h1>Add \"/(celsius)\" to the route see your convention result!</h1>"
+
+
 @app.route('/f/<celsius>')
-def greet(celsius=""):
+def convert(celsius=""):
     """Return a string message with helpful text and show the celsius - fahrenheit converting."""
     message = f"<h1>Celsius of {celsius} to fahrenheit is " \
               f"{str(celsius_to_fahrenheit(celsius))}</h1>"
